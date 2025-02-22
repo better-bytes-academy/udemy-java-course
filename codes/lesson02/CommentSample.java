68336a2293d261b5d4477d8664e518521d02ef92
Dong <donglev91.work@gmail.com>
2025-02-22 15:06:08 +0700

    Add lesson 2 - comment

---
+++ codes/lesson02/CommentSample.java
@@ -0,0 +1,20 @@
+package lesson02;
+
+/**
+ * Sample class for Comment
+ * */
+public class CommentSample {
+    /**
+     * Main method
+     * @param args tham số dòng lệnh
+     * */
+    public static void main(String[] args) {
+        // single line comment here
+        System.out.println("Hello world!"); // short notice
+
+        /*
+        * Comment dòng 1
+        * Comment dòng 2
+        * */
+    }
+}
