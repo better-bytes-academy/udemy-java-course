8b3a2b39e3c379c6a2920f7ff1f9599c5ba7a53b
Dong <donglev91.work@gmail.com>
2025-02-24 07:44:51 +0700

    add sample code

--- codes/lesson02/VariableSample.java
+++ codes/lesson02/VariableSample.java
@@ -1,4 +1,24 @@
 package lesson02;
 
 public class VariableSample {
+    public static void main(String[] args) {
+        // <kieu gia tri: int, String> <ten bien>;
+        // <kieu gia tri> <ten bien> = <gia tri khoi tao>;
+
+        int _$studentAge123 = 123;
+        String _$StudentAge123;
+        // int 0Student = 123;
+
+        String fullName = "Nguyen Van A";
+        boolean isLastSeen = true;
+        isLastSeen = false;
+
+        // int goto = 123;
+
+        // final <kieu du lieu> <ten hang>;
+        // final <kieu du lieu> <ten hang> = <gia tri khoi tao>;
+        final int MAX_USERS = 1000;
+        // MAX_USERS = 1000;
+
+    }
 }
