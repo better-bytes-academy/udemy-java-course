0380ec41d2d8e1ca613e5a4858ed3b731714cfd9
Dong <donglev91.work@gmail.com>
2025-03-01 17:55:41 +0700

    add sample code

--- codes/lesson02/InOutSample.java
+++ codes/lesson02/InOutSample.java
@@ -1,45 +1,32 @@
 package lesson02;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
+
 import java.util.Scanner;
 
 public class InOutSample {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); // Tạo đối tượng Scanner
+        System.out.print("Hello");
+        System.out.println(" World");
+        System.out.println("-----");
 
-        System.out.print("Nhập tuổi của bạn: ");
-        int age = scanner.nextInt(); // Đọc số nguyên
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Nhập điểm trung bình: ");
-        double score = scanner.nextDouble(); // Đọc số thực
+        System.out.println("Nhap ten:");
+        String name = scanner.nextLine();
+
+        System.out.println("Nhap tuoi:");
+        int age = scanner.nextInt();
+
+        System.out.println("Nhap diem trung binh:");
+        double score = scanner.nextDouble();
+
+        System.out.println("Nhap dia chi:");
         scanner.nextLine();
+        String address = scanner.nextLine();
+        System.out.println(address);
 
-        System.out.print("Nhập tên của bạn: ");
-        String name = scanner.nextLine(); // Đọc cả dòng
-
-        System.out.println("Xin chào, " + name + "! Bạn " + age + " tuổi, điểm trung bình: " + score);
-
-        scanner.close(); // Đóng Scanner để giải phóng tài nguyên
-
-//        try {
-//            // Tạo file log cho error
-//            File errorLog = new File("error.log");
-//            PrintStream errorStream = new PrintStream(errorLog);
-//
-//            // Chuyển hướng System.err vào file
-//            System.setErr(errorStream);
-//
-//            // In ra console (stdout)
-//            System.out.println("Đây là thông báo thông thường (System.out)");
-//
-//            // In lỗi ra file (stderr)
-//            System.err.println("Đây là thông báo lỗi (System.err)");
-//
-//            // Đóng stream sau khi sử dụng
-//            errorStream.close();
-//        } catch (FileNotFoundException e) {
-//            System.out.println("Không thể tạo file log: " + e.getMessage());
-//        }
+        System.out.println("Ten: " + name + "; tuoi: " + age + "; Diem: " + score);
+        System.out.printf("Ten: %s; Tuoi: %d; Diem: %f %n", name, age, score);
+        System.out.printf("Ten: %-15s; Tuoi: %-10d; Diem: %-5.1f", name, age, score);
+        scanner.close();
     }
 }
